88d102c9df55df57b4f78514c5bd6e7d1d0d664c
Nico Alt <nicoalt@gmx.de>
2014-10-09 21:04:30 +0200

    added function to add own subjects

--- HA-Manager/src/de/nico/ha_manager/MainActivity.java
+++ HA-Manager/src/de/nico/ha_manager/MainActivity.java
@@ -76,6 +76,10 @@ public class MainActivity extends Activity {
 	        	mainisopen = false;
 	            return true;
 	            
+	        case R.id.action_resetsubjects:
+	        	delete_subjects();
+	            return true;
+	            
 	        case R.id.action_delete:
 	        	delete_all();
 	            return true;
@@ -94,6 +98,11 @@ public class MainActivity extends Activity {
 	        	setContentView(R.layout.activity_add);
 	        	setTitle(getString(R.string.action_add));
 	        	mainisopen = false;
+
+	    		Spinner subspin = (Spinner) findViewById(R.id.spinsubject);
+	    		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, subjects);
+	    	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    	    subspin.setAdapter(adapter);
 	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item); 
@@ -137,7 +146,7 @@ public class MainActivity extends Activity {
 			deleted = false;
 		}*/
 		
-		Spinner subject_sp = (Spinner) findViewById(R.id.subject);
+		Spinner subject_sp = (Spinner) findViewById(R.id.spinsubject);
 		Spinner until_sp = (Spinner) findViewById(R.id.until);
 		EditText homework_edit = (EditText) findViewById(R.id.edit_text_subject_add);
 		
@@ -191,6 +200,33 @@ public class MainActivity extends Activity {
 		update();
 	}
 	
+	public void delete_subjects () {
+		AlertDialog.Builder delete_it = new AlertDialog.Builder(this);
+		delete_it.setTitle(getString(R.string.delete));
+		delete_it.setMessage(getString(R.string.really_subs));
+		delete_it.setPositiveButton((getString(R.string.yes)),
+		   new DialogInterface.OnClickListener() {
+			 
+		      public void onClick(DialogInterface dialog, int which) {
+		  		setContentView(R.layout.activity_main);
+				setTitle(getString(R.string.homework));
+				mainisopen = true;
+				setDefaultSubjects();
+				update();
+		    }
+		   });
+
+		delete_it.setNegativeButton((getString(R.string.no)),
+		   new DialogInterface.OnClickListener() {
+			 
+		      public void onClick(DialogInterface dialog, int which) {
+		  		//do nothing
+		    }
+		   });
+		AlertDialog delete_dialog = delete_it.create();
+		delete_dialog.show();
+	}
+	
 	public void delete_all() {
 		AlertDialog.Builder delete_it = new AlertDialog.Builder(this);
 		delete_it.setTitle(getString(R.string.delete));
@@ -229,24 +265,35 @@ public class MainActivity extends Activity {
 	}
 	
 	public void addSubject (View v) {
-		// DOES NOT WORK!!! LISTVIEW CRASHES...
-		//TODO: add subject (sub to subjects)
 		EditText new_sub = (EditText) findViewById(R.id.edit_text_subject_add);
 		String sub = new_sub.getText().toString();
 		
-    	String [] subjects = getResources().getStringArray(R.array.subjects);
-		SharedPreferences.Editor editor = prefs.edit();
+		//close keyboard
+				InputMethodManager imm = (InputMethodManager)getSystemService(
+			  		      Context.INPUT_METHOD_SERVICE);
+			  		imm.hideSoftInputFromWindow(new_sub.getWindowToken(), 0);
+				
+			  	
+		int size = prefs.getInt("subjects" + "_size", 0);
+		subjects = new String[size];
+		for(int i=0;i<size;i++) {
+			subjects[i] = prefs.getString("subjects" + "_" + i, null);
+		}
+    	SharedPreferences.Editor editor = prefs.edit();
 		editor.putInt("subjects" +"_size", subjects.length);
 		for(int i = 0; i < subjects.length; i++) {
 			editor.putString("subjects" + "_" + i, subjects[i]);
 		}
-		int size = prefs.getInt("subjects" + "_size", 0);
-		size = size + 1;
 		editor.putString("subjects" + "_" + size, sub);
 		editor.putInt("subjects" +"_size", subjects.length + 1);
 		editor.commit();
 		getSubjects();
 		setSubjects();
+		goHome();
+		
+		//Toast
+		String sAdded = getString(R.string.added);
+		Toast.makeText(MainActivity.this, sub + " " + sAdded, Toast.LENGTH_SHORT).show();
 	}
 	
 	public void setDefaultSubjects () {
@@ -260,17 +307,21 @@ public class MainActivity extends Activity {
 		getSubjects();
 	}
 	
+	public void goHome () {
+		mainisopen = true;
+		setContentView(R.layout.activity_main);
+		setTitle(getString(R.string.homework));
+		
+        //update list view			
+		HomeworkList.clear();
+		update();
+	}
+	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 
 		if (keyCode == KeyEvent.KEYCODE_BACK && mainisopen == false){
-			mainisopen = true;
-			setContentView(R.layout.activity_main);
-			setTitle(getString(R.string.homework));
-			
-            //update list view			
-			HomeworkList.clear();
-			update();
+			goHome();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
